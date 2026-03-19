86cc559575b100ed37ceddef742bffa52f7308e4
kipngenokev <kkipngeno409@gmail.com>
2026-03-19 21:33:12 +0300

    connected to mysql db

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from './config/env.js'
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase  from './database/mongodb.js';
 
 
 
@@ -18,8 +19,9 @@ app.get('/',(req,res)=>{
     res.send('Welcome to the Subsscription Tracker API');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+    await connectToDatabase();
 })
 
 export default app;
